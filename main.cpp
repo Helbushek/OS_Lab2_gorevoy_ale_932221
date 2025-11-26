3dca753243fadd393689f74dbc3d173c406c467f
Helbushek <gorevaya134@gmail.com>
2025-11-26 21:05:20 +0700

    polished + finished last points from task

--- main.cpp
+++ main.cpp
@@ -1,187 +1,161 @@
-#include <iostream>
-#include <thread>
-
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
 #include <arpa/inet.h>
-
-#include <chrono>
-#include <cstring>
-
 #include <signal.h>
-#include <csignal>
 
-int serverSocket = -1;
-int clientSocket = -1;
-constexpr int PortNumber = 8009;
-
-bool isFinished = false;
-bool isConnected = false;
+constexpr int PORT = 8009;
 
 volatile sig_atomic_t wasSigHup = 0;
+int listen_fd = -1;
+int client_fd = -1;
 
-sigset_t origMask;
-sigset_t blockedMask;
-
-void Server()
+void sigHupHandler(int) 
 {
-    while (!isFinished)
+    wasSigHup = 1;
+}
+
+int main() 
+{
+    struct sigaction sa;
+    sigaction(SIGHUP, nullptr, &sa);
+    sa.sa_handler = sigHupHandler;
+    sa.sa_flags |= SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGHUP, &sa, nullptr);
+
+    sigset_t blockedMask, origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+    if (sigprocmask(SIG_BLOCK, &blockedMask, &origMask) == -1) 
     {
-        fd_set readfds;
-        FD_ZERO(&readfds);
-    
-        if (serverSocket == -1)
+        perror("sigprocmask");
+        return EXIT_FAILURE;
+    }
+
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1) 
+    { 
+        perror("socket"); 
+        return EXIT_FAILURE; 
+    }
+
+    int opt = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    if (bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) 
+    {
+        perror("bind");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
+
+    if (listen(listen_fd, SOMAXCONN) == -1) 
+    {
+        perror("listen");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (true) 
+    {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(listen_fd, &fds);
+        int max_fd = listen_fd;
+
+        if (client_fd != -1) 
         {
-            break;
+            FD_SET(client_fd, &fds);
+            if (client_fd > max_fd) max_fd = client_fd;
         }
 
-        FD_SET(serverSocket, &readfds);
-        int maxFd = serverSocket;
-
-        if (isConnected && clientSocket != -1)
+        int ret = pselect(max_fd + 1, &fds, nullptr, nullptr, nullptr, &origMask);
+        if (ret == -1) 
         {
-            FD_SET(clientSocket, &readfds);
-            if (clientSocket > maxFd) maxFd = clientSocket;
-        }
-
-        int ready = pselect(maxFd + 1, &readfds, nullptr, nullptr, nullptr, &origMask);
-
-        if (ready == -1)
-        {
-            if (errno == EINTR)
+            if (errno == EINTR) 
             {
-                if (wasSigHup)
+                if (wasSigHup) 
                 {
-                    std::cout << "Received SIGHUP" << std::endl;
-                    isFinished = true;
+                    printf("Received SIGHUP\n");
                     wasSigHup = 0;
                 }
                 continue;
             }
-
-            std::cerr << strerror(errno) << std::endl;
+            perror("pselect");
             break;
         }
 
-        if (FD_ISSET(serverSocket, &readfds) && !isConnected)
+        if (FD_ISSET(listen_fd, &fds)) 
         {
-            sockaddr_in cliaddr{};
-            socklen_t len = sizeof(cliaddr);
-            int fd = accept(serverSocket, (sockaddr*)&cliaddr, &len);
-
-            if (fd == -1)
+            struct sockaddr_in cli{};
+            socklen_t len = sizeof(cli);
+            int fd = accept(listen_fd, (struct sockaddr*)&cli, &len);
+            if (fd == -1) 
             {
-                std::cerr << "Error occurred: " << strerror(errno) << std::endl;
+                perror("accept");
                 continue;
             }
 
-            clientSocket = fd;
-            isConnected = true;
             char ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &cliaddr.sin_addr, ip, sizeof(ip));
-            std::cout << "Client connected: " << ip << ":" << ntohs(cliaddr.sin_port) << std::endl;
+            inet_ntop(AF_INET, &cli.sin_addr, ip, sizeof(ip));
+            printf("New connection from %s:%d\n", ip, ntohs(cli.sin_port));
+
+            if (client_fd == -1) 
+            {
+                client_fd = fd;
+                printf("Connection accepted\n");
+            } 
+            else 
+            {
+                printf("Rejecting additional connection from %s:%d\n", ip, ntohs(cli.sin_port));
+                close(fd);
+            }
         }
 
-        if (isConnected && FD_ISSET(clientSocket, &readfds))
+        if (client_fd != -1 && FD_ISSET(client_fd, &fds)) 
         {
-            char buf[1024];
-            ssize_t n = recv(clientSocket, buf, sizeof(buf), 0);
+            char buf[4096];
+            ssize_t n = recv(client_fd, buf, sizeof(buf), 0);
 
-            if (n == 0)
+            if (n > 0) 
             {
-                std::cout << "Client disconnected" << std::endl;
-                close(clientSocket);
-                clientSocket = -1;
-                isConnected = false;
-            }
-            else if (n < 0)
+                printf("Received %zd bytes\n", n);
+            } 
+            else if (n == 0) 
             {
-                std::cerr << "Error occurred: " << strerror(errno) << std::endl;
-                close(clientSocket);
-                clientSocket = -1;
-                isConnected = false;
-            }
-            else
+                printf("Client closed connection\n");
+                close(client_fd);
+                client_fd = -1;
+            } 
+            else 
             {
-                std::cout << "Received " << std::to_string(n) << " bytes: " << std::string(buf, n) << std::endl;
+                perror("recv");
+                close(client_fd);
+                client_fd = -1;
             }
         }
     }
-}
 
-bool InitializeServer()
-{
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket == -1)
+    if (listen_fd != -1) 
     {
-        std::cerr << "Couldn`t open server`s socket" << std::endl; 
-        return false;
+        close(listen_fd);
     }
-
-    int opt = 1;
-    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    sockaddr_in serverAddress{};
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY; 
-    serverAddress.sin_port = htons(PortNumber);
     
-    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) 
+    if (client_fd != -1) 
     {
-        std::cerr << "Couldn`t bind socket to address" << std::endl;
-        std::cerr << strerror(errno) << std::endl;
-        return false;
+        close(client_fd);
     }
-
-    if (listen(serverSocket, SOMAXCONN) == -1) 
-    {
-        std::cerr << "Error during socket listen" << std::endl;
-        return false;
-    }
-
-    return true;
-}
-
-extern "C" void SignalHandler(int)
-{
-    wasSigHup = 1; 
-}
-
-int main()
-{
-    sigemptyset(&blockedMask);
-    sigaddset(&blockedMask, SIGHUP);
-
-    if (sigprocmask(SIG_BLOCK, &blockedMask, &origMask) == -1)
-    {
-        std::cout << "Sigprocmask failed: " << strerror(errno) << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    struct sigaction sa{};
-    sigemptyset(&sa.sa_mask);
-
-    sa.sa_handler = SignalHandler;
-    sa.sa_flags = 0;
-
-    if (sigaction(SIGHUP, &sa, NULL) == -1) {
-        std::cerr << "Error registering SIGINT handler" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    if (!InitializeServer())
-    {
-        return EXIT_FAILURE;
-    }
-
-    std::thread serverThread(Server);
-    serverThread.join();
-    close(serverSocket);
-    if (clientSocket != -1)
-    {
-        close(clientSocket);
-    }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
